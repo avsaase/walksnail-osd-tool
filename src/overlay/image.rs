05d07ff4730952df512250e512e5016eac89f7cc
Alexander van Saase <avsaase@gmail.com>
2023-03-21 20:45:05 +0100

    Small overlay optimization

--- src/overlay/image.rs
+++ src/overlay/image.rs
@@ -9,20 +9,20 @@ pub fn overlay_osd_on_image(
     horizontal_offset: i32,
     vertical_offset: i32,
 ) {
-    let blank_image = font.characters.get(0).unwrap();
     // TODO: check if this can be run in parallel
     for character in &osd_frame.glyphs {
         if character.index == 0 {
             continue;
         }
-        let character_image = font.characters.get(character.index as usize).unwrap_or(blank_image);
-        let grid_position = &character.grid_position;
-        let (char_width, char_height) = character_image.dimensions();
-        overlay(
-            rgba_image,
-            character_image,
-            (grid_position.x as i32 * char_width as i32 + horizontal_offset).into(),
-            (grid_position.y as i32 * char_height as i32 + vertical_offset).into(),
-        )
+        if let Some(character_image) = font.characters.get(character.index as usize) {
+            let grid_position = &character.grid_position;
+            let (char_width, char_height) = character_image.dimensions();
+            overlay(
+                rgba_image,
+                character_image,
+                (grid_position.x as i32 * char_width as i32 + horizontal_offset).into(),
+                (grid_position.y as i32 * char_height as i32 + vertical_offset).into(),
+            )
+        }
     }
 }
