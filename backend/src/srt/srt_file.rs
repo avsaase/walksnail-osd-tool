0264a04d16a22c124f7e6a61f9fffe337f48a76e
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2024-02-09 15:31:22 +0100

    Initial attempt at adding debug srt processing
    Currently it can parse debug srt, but is bypassing srt parsing completelly.

--- backend/src/srt/srt_file.rs
+++ backend/src/srt/srt_file.rs
@@ -2,13 +2,14 @@ use std::{path::PathBuf, time::Duration};
 
 use derivative::Derivative;
 
-use super::{error::SrtFileError, frame::SrtFrame, SrtFrameData};
+use super::{error::SrtFileError, frame::SrtFrame, SrtFrameData, frame::SrtDebugFrameData};
 
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct SrtFile {
     pub file_path: PathBuf,
     pub has_distance: bool,
+    pub has_debug: bool,
     pub duration: Duration,
     #[derivative(Debug = "ignore")]
     pub frames: Vec<SrtFrame>,
@@ -18,15 +19,22 @@ impl SrtFile {
     #[tracing::instrument(ret, err)]
     pub fn open(path: PathBuf) -> Result<Self, SrtFileError> {
         let mut has_distance = false;
+        let mut has_debug = false;
         let srt_frames = srtparse::from_file(&path)?
             .iter()
             .map(|i| -> Result<SrtFrame, SrtFileError> {
-                let data: SrtFrameData = i.text.parse()?;
+                //let debugData = i.text.parse()?;
+                //has_debug = true;
+                let data: SrtFrameData = SrtFrameData {
+                    signal: 0, channel: 0, flight_time: 0, sky_bat: 0.0, ground_bat: 0.0, latency: 0, bitrate_mbps: 0.0, distance: 0
+                };
+                //let data: SrtFrameData = i.text.parse()?;
                 has_distance |= data.distance > 0;
                 Ok(SrtFrame {
                     start_time_secs: i.start_time.into_duration().as_secs_f32(),
                     end_time_secs: i.end_time.into_duration().as_secs_f32(),
                     data,
+                    //debugData,
                 })
             })
             .collect::<Result<Vec<_>, _>>()?;
@@ -36,6 +44,7 @@ impl SrtFile {
         Ok(Self {
             file_path: path,
             has_distance,
+            has_debug,
             duration,
             frames: srt_frames,
         })
